81e21eab90d6591916a9ca8d387514e8a1c45367
xy173 <xing0047@e.ntu.edu.sg>
2022-01-04 09:33:40 +0800

    adamine

--- train.py
+++ train.py
@@ -5,24 +5,27 @@ import torch.optim as optim
 import sys
 import random
 from tqdm import tqdm
+from typing import Tuple
+from torch import Tensor
+import time
 
 ### global settings
 # model
 IN = 2048
-OUT = 1024
+OUT = 64
 
 # train
 GPU_ID = 0
 LEARNING_RATE = 1e-4
 MOMENTUM = 0.0005
 WEIGHT_DECAY = 0.9 
-BATCH_SIZE = 1  
-MARGIN = 0.3
+BATCH_SIZE = 32 
+MARGIN = 0.5
 ITERS = 120000
 NUM_WORKERS = 0
 
 # save
-SAVE_EVERY = 2000
+SAVE_EVERY = 200
 SNAPSHOT = 'snapshots'
 
 ### dataset
@@ -53,7 +56,7 @@ class Dataset(torch.utils.data.Dataset):
         else:
             pos_pth = anchor_pth
         # neg list
-        neg_idx_list = self.data.keys()
+        neg_idx_list = list(self.data.keys())
         neg_idx_list.remove(idx)
         neg_idx = random.choice(neg_idx_list)
         neg_list = self.data[neg_idx]
@@ -88,6 +91,36 @@ def loss_calc(anchor, pos, neg, margin):
     return triplet_loss(anchor, pos, neg)
 
 
+### adaptive mining
+def ada_mine(model, dataloader_iter) -> Tuple[Tensor, Tensor, Tensor, float]:
+    # record time   
+    start = time.time()
+
+    anchor_lst = []
+    pos_lst = []
+    neg_lst = []
+    
+    while len(anchor_lst) < BATCH_SIZE:
+        with torch.no_grad():
+            _, data = dataloader_iter.__next__()
+            anchor, pos, neg = data
+            # propagate
+            anchor_out, pos_out, neg_out = model(anchor.cuda(device), pos.cuda(device), neg.cuda(device))
+            if loss_calc(anchor_out, pos_out, neg_out, margin=MARGIN) > 0:
+                anchor_lst.append(anchor)
+                pos_lst.append(pos)
+                neg_lst.append(neg)
+    
+    anchor = torch.cat(anchor_lst)
+    pos = torch.cat(pos_lst)
+    neg = torch.cat(neg_lst)
+    
+    assert anchor.shape[0] == BATCH_SIZE
+    assert pos.shape[0] == BATCH_SIZE
+    assert neg.shape[0] == BATCH_SIZE
+    
+    ada_t = time.time() - start
+    return anchor, pos, neg, ada_t
 ### main
 if __name__ == '__main__':
     device = GPU_ID
@@ -97,7 +130,7 @@ if __name__ == '__main__':
     
     # dataloader
     dataloader = torch.utils.data.DataLoader(dataset,
-					     batch_size=BATCH_SIZE,
+					     batch_size=1,
 					     num_workers=NUM_WORKERS,
  					     shuffle=True,
 					     pin_memory=True)
@@ -124,14 +157,16 @@ if __name__ == '__main__':
         # adjust_learning_rate(optimizer, i_iter)
         # load data
         _, data = dataloader_iter.__next__()
-        anchor, pos, neg = data
+        # adaptive mining
+        anchor, pos, neg, ada_t = ada_mine(model, dataloader_iter)
         # propagate
         anchor_out, pos_out, neg_out = model(anchor.cuda(device), pos.cuda(device), neg.cuda(device))
         # loss
         loss = loss_calc(anchor_out, pos_out, neg_out, margin=MARGIN)
         loss.backward()
         # print
-        print('Iter {}: {:.4f}'.format(i_iter, loss.item()))
+        print('Iter {}: {:.4f}, Mining Cost: {:.4f} sec'.format(i_iter, loss.item(), ada_t))
+        
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
         optimizer.step()
         # save
